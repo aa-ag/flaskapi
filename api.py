08c3c49afb6739375f651bfb71e62fafe04ed1c0
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-18 21:44:23 -0600

    all CRUD operations working for Person class

--- api.py
+++ api.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
+
 ##--- init API ---##
 app = Flask(__name__)
 api = Api(app)
@@ -22,7 +23,6 @@ PEOPLE = {
 parser = reqparse.RequestParser()
 
 class PeopleList(Resource):
-    
     def get(self):
         return PEOPLE
 
@@ -40,11 +40,42 @@ class PeopleList(Resource):
         }
         return PEOPLE[person_id], 201
 
+class Person(Resource):
+    '''
+    CRUD operations for each person in PEOPLE class
+    '''
+    def get(self, person_id):
+        if person_id not in PEOPLE:
+            return "Not found", 404
+        else:
+            return PEOPLE[person_id]
+
+    def put(self, person_id):
+        parser.add_argument('name')
+        parser.add_argument('age')
+        parser.add_argument('favorite_wine')
+        args = parser.parse_args()
+        if person_id not in PEOPLE:
+            return "Not found", 404
+        else:
+            person = PEOPLE[person_id]
+            person['name'] = args['name'] if args['name'] != None else person['name']
+            person['age'] = args['age'] if args['age'] != None else person['age']
+            person['favorite_wine'] = args['favorite_wine'] if args['favorite_wine'] != None else person['favorite_wine']
+            return person, 200
+
+    def delete(self, person_id):
+        if person_id not in PEOPLE:
+            return "Not found", 404
+        else:
+            del PEOPLE[person_id]
+            return '', 204
+
 
 ##--- route(s) ---##
 
 api.add_resource(PeopleList, '/people/')
-# api.add_resource(Student, '/people/<person_id>')
+api.add_resource(Person, '/people/<person_id>')
 
 
 ##--- run app in debug mode ---##
